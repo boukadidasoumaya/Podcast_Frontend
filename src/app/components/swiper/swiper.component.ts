ae096b81e8836a65f0b7b6a0830bb3aa16676f95
mabahej <mabahej.benhassine@insat.ucar.tn>
2025-01-18 23:09:25 +0100

    added  seperate section component fixe css issues

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SectionCustomComponent } from "../section-custom/section-custom.component";
 
 @Component({
   selector: 'app-swiper',
   standalone: true,
-  imports: [],
+  imports: [SectionCustomComponent],
   templateUrl: './swiper.component.html',
   styleUrl: './swiper.component.css'
 })
