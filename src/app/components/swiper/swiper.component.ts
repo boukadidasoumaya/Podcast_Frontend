8cb779f0a59630becf2eaddb3f6376d9055a3fe1
mabahej <mabahej.benhassine@insat.ucar.tn>
2025-01-18 23:59:39 +0100

    fixed topics

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -8,92 +8,76 @@ import { ViewportScroller } from '@angular/common'; // Import ViewportScroller
 @Component({
   selector: 'app-swiper',
   standalone: true,
-  imports: [CommonModule ,SectionCustomComponent],
+  imports: [CommonModule, SectionCustomComponent],
   templateUrl: './swiper.component.html',
   styleUrls: ['./swiper.component.css'],
   providers: [DecimalPipe] // Add DecimalPipe to providers
 })
 export class SwiperComponent {
-  // Array to hold the podcast data
-  podcasts = [
-    {
-      title: 'Modern Vintage',
-      duration: '50 Minutes',
-      episode: 15,
-      image: 'images/podcast/11683425_4790593.jpg',
-      profileImage: 'images/profile/woman-posing-black-dress-medium-shot.jpg',
-      name: 'Elsa',
-      type: 'Influencer',
-      listeners: 120000,
-      likes: 42500,
-      comments: 11000,
-      downloads: 50000
+  currentPage = 1;
+  itemsPerPage = 4;  // Adjust according to your needs
+  totalItems = 0;
+  
+  pagedTopics = [
+    { 
+      title: 'Productivity', 
+      image: 'images/topics/physician-consulting-his-patient-clinic.jpg', 
+      episodes: 50
     },
-    {
-      title: 'Daily Talk',
-      duration: '15 Minutes',
-      episode: 45,
-      image: 'images/podcast/12577967_02.jpg',
-      profileImage: 'images/profile/handsome-asian-man-listening-music-through-headphones.jpg',
-      name: 'William',
-      type: 'Vlogger',
-      listeners: 140000,
-      likes: 22400,
-      comments: 16000,
-      downloads: 62000
-    },{
-      title: 'Modern Vintage',
-      duration: '50 Minutes',
-      episode: 15,
-      image: 'images/podcast/11683425_4790593.jpg',
-      profileImage: 'images/profile/woman-posing-black-dress-medium-shot.jpg',
-      name: 'Elsa',
-      type: 'Influencer',
-      listeners: 120000,
-      likes: 42500,
-      comments: 11000,
-      downloads: 50000
+    { 
+      title: 'Technician', 
+      image: 'images/topics/repairman-doing-air-conditioner-service.jpg', 
+      episodes: 12 
     },
-    {
-      title: 'Daily Talk',
-      duration: '15 Minutes',
-      episode: 45,
-      image: 'images/podcast/12577967_02.jpg',
-      profileImage: 'images/profile/handsome-asian-man-listening-music-through-headphones.jpg',
-      name: 'William',
-      type: 'Vlogger',
-      listeners: 140000,
-      likes: 22400,
-      comments: 16000,
-      downloads: 62000
+    { 
+      title: 'Mindfulness', 
+      image: 'images/topics/woman-practicing-yoga-mat-home.jpg', 
+      episodes: 35
+    },
+    { 
+      title: 'Cooking', 
+      image: 'images/topics/delicious-meal-with-sambal-arrangement.jpg', 
+      episodes: 12 
+    }, 
+    { 
+      title: 'Productivity', 
+      image: 'images/topics/physician-consulting-his-patient-clinic.jpg', 
+      episodes: 50
+    },
+    { 
+      title: 'Technician', 
+      image: 'images/topics/repairman-doing-air-conditioner-service.jpg', 
+      episodes: 12 
+    },
+    { 
+      title: 'Mindfulness', 
+      image: 'images/topics/woman-practicing-yoga-mat-home.jpg', 
+      episodes: 35
+    },
+    { 
+      title: 'Cooking', 
+      image: 'images/topics/delicious-meal-with-sambal-arrangement.jpg', 
+      episodes: 12 
     }
-    // Add more podcasts here...
   ];
 
-  // Pagination properties
-  currentPage = 1;
-  itemsPerPage = 2;
-  totalItems = this.podcasts.length;
-
-  constructor(private decimalPipe: DecimalPipe, private viewportScroller: ViewportScroller) {}
-
-  // Method to get the current page items
-  get pagedPodcasts() {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    return this.podcasts.slice(startIndex, startIndex + this.itemsPerPage);
+  ngOnInit(): void {
+    // Set the total number of items based on the length of pagedTopics
+    this.totalItems = this.pagedTopics.length;
   }
 
-  // Method to handle page change and smooth scroll
-  changePage(pageNumber: number) {
-    this.currentPage = pageNumber;
-    // Scroll to the top of the page smoothly
-    this.viewportScroller.scrollToPosition([0, 0]);
-  }
   get totalPages() {
     return Math.ceil(this.totalItems / this.itemsPerPage);
   }
-  // Method to format numbers with DecimalPipe
-  formatNumber(value: number) {
-    return this.decimalPipe.transform(value);
+
+  changePage(pageNumber: number) {
+    this.currentPage = pageNumber;
+  }
+
+  // Calculate the topics to display based on the current page and items per page
+  get displayedTopics() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.pagedTopics.slice(startIndex, endIndex);
   }
 }
