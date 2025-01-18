a420c531bfb40a87da16700ed76429d39d4aa061
mabahej <mabahej.benhassine@insat.ucar.tn>
2025-01-18 22:08:39 +0100

    added swiper still needs css changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardEpisodeComponent } from './components/card-episode/card-episode.component';
 import { NgModule } from '@angular/core';
 import { TrendingEpisodesComponent } from './components/trending-episodes/trending-episodes.component';
+import { SwiperComponent } from './components/swiper/swiper.component';
 
 export const routes: Routes = [
-  {path:'',component:TrendingEpisodesComponent}
+  {path:'',component:TrendingEpisodesComponent},
+  {path:'swiper',component:SwiperComponent},
 ];
+
 
