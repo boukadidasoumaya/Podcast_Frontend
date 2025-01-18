3a656b9c02b0c090f625b294924a39833fb7aa50
mabahej <mabahej.benhassine@insat.ucar.tn>
2025-01-18 23:42:38 +0100

    added smoth scrolling

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -3,6 +3,8 @@ import { SectionCustomComponent } from "../section-custom/section-custom.compone
 import { DecimalPipe } from '@angular/common'; // Import DecimalPipe
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ViewportScroller } from '@angular/common'; // Import ViewportScroller
+
 @Component({
   selector: 'app-swiper',
   standalone: true,
@@ -39,30 +41,58 @@ export class SwiperComponent {
       likes: 22400,
       comments: 16000,
       downloads: 62000
+    },{
+      title: 'Modern Vintage',
+      duration: '50 Minutes',
+      episode: 15,
+      image: 'images/podcast/11683425_4790593.jpg',
+      profileImage: 'images/profile/woman-posing-black-dress-medium-shot.jpg',
+      name: 'Elsa',
+      type: 'Influencer',
+      listeners: 120000,
+      likes: 42500,
+      comments: 11000,
+      downloads: 50000
+    },
+    {
+      title: 'Daily Talk',
+      duration: '15 Minutes',
+      episode: 45,
+      image: 'images/podcast/12577967_02.jpg',
+      profileImage: 'images/profile/handsome-asian-man-listening-music-through-headphones.jpg',
+      name: 'William',
+      type: 'Vlogger',
+      listeners: 140000,
+      likes: 22400,
+      comments: 16000,
+      downloads: 62000
     }
     // Add more podcasts here...
   ];
 
   // Pagination properties
   currentPage = 1;
-  itemsPerPage = 2;  // You can change this value to control how many items per page
+  itemsPerPage = 2;
   totalItems = this.podcasts.length;
+
+  constructor(private decimalPipe: DecimalPipe, private viewportScroller: ViewportScroller) {}
 
   // Method to get the current page items
   get pagedPodcasts() {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    console.log(this.podcasts);
     return this.podcasts.slice(startIndex, startIndex + this.itemsPerPage);
   }
 
-  // Method to handle page change
+  // Method to handle page change and smooth scroll
   changePage(pageNumber: number) {
     this.currentPage = pageNumber;
+    // Scroll to the top of the page smoothly
+    this.viewportScroller.scrollToPosition([0, 0]);
   }
-
-  constructor(private decimalPipe: DecimalPipe) {} // Inject DecimalPipe
-
-  // Method to format numbers with DecimalPipe (e.g., listeners, likes, etc.)
+  get totalPages() {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+  // Method to format numbers with DecimalPipe
   formatNumber(value: number) {
     return this.decimalPipe.transform(value);
   }
