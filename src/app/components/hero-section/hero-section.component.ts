d04725cb882c5424e2543e27807208ffc5ff5fed
arijzaidi <arij.zaidi@insat.ucar.tn>
2025-01-20 19:11:16 +0100

    hero section done

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeroCardComponent } from '../hero-card/hero-card.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { CommonModule } from '@angular/common';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 declare var $: any;
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [HeroCardComponent,CommonModule],
+  imports: [HeroCardComponent,CommonModule,CarouselModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
@@ -22,5 +24,35 @@ export class HeroSectionComponent {
   //     });
   //   });
   // }
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    pullDrag: false,
 
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+
+    center: true,
+
+
+    dots: true,
+    dotsEach: 1,
+
+    nav: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+  };
 }
