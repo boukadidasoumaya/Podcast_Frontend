9825322fa20648bc352a40dcc59b3b7b0c098f4e
mabahej <mabahej.benhassine@insat.ucar.tn>
2025-01-21 12:06:10 +0100

    umerge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,30 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardEpisodeComponent } from './components/card-episode/card-episode.component';
 import { NgModule } from '@angular/core';
 import { TrendingEpisodesComponent } from './components/trending-episodes/trending-episodes.component';
-import { ProfilComponent } from './components/profil/profil.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { DetailsPodcastComponent } from './components/details-podcast/details-podcast.component';
-import { FooterComponent } from './components/footer/footer.component';
-
 import { SwiperComponent } from './components/swiper/swiper.component';
 import { TopicsComponent } from './components/topics/topics.component';
 import { TestComponent } from './components/test/test.component';
-import { LoginComponent } from './components/login/login.component';
-import { LatestEpisodesComponent } from './components/latest-episodes/latest-episodes.component';
-import { ContactComponent } from './components/contact/contact.component';
-
 export const routes: Routes = [
-  {path:'',component:NavbarComponent},
-  {path:'trending',component:TrendingEpisodesComponent},
-  {path:'latest', component:LatestEpisodesComponent},
-  {path:'profil',component:ProfilComponent},
-  {path:'details-podcast',component:DetailsPodcastComponent},
-  {path:'topics',component:TopicsComponent},
-  {path:'contact',component:ContactComponent},
-
-  {path:'footer',component:FooterComponent},
+  {path:'',component:TrendingEpisodesComponent},
   {path:'swiper',component:TestComponent},
-  {path:'login' , component:LoginComponent}
 ];
 
 
--- src/app/components/trending-episodes/trending-episodes.component.ts
+++ src/app/components/trending-episodes/trending-episodes.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CardEpisodeComponent } from '../card-episode/card-episode.component';
 import { CommonModule } from '@angular/common';  // Importation nécessaire
-import { SectionCustomComponent } from '../section-custom/section-custom.component';
 
 @Component({
   selector: 'app-trending-episodes',
   standalone: true,
-  imports: [CardEpisodeComponent, CommonModule,SectionCustomComponent],  // Ajoute CommonModule ici
+  imports: [CardEpisodeComponent, CommonModule],  // Ajoute CommonModule ici
   templateUrl: './trending-episodes.component.html',
   styleUrls: ['./trending-episodes.component.css']
 })
@@ -18,10 +17,7 @@ export class TrendingEpisodesComponent {
       description: 'Lorem Ipsum dolor sit amet consectetur',
       profileImage: 'assets/images/profile/woman-posing-black-dress-medium-shot.jpg',
       profileName: 'Elsa',
-      profileRole: 'Influencer',
-      listensCount: '120k',   // Nombre de lectures
-      likesCount: '2.3k',     // Nombre de likes
-      commentsCount: '500'    // Nombre de commentaires
+      profileRole: 'Influencer'
     },
     {
       imagePath: 'assets/images/podcast/27670664_7369753.jpg',
@@ -29,10 +25,7 @@ export class TrendingEpisodesComponent {
       description: 'Lorem Ipsum dolor sit amet consectetur',
       profileImage: 'assets/images/profile/cute-smiling-woman-outdoor-portrait.jpg',
       profileName: 'Taylor',
-      profileRole: 'Creator',
-      listensCount: '200k',
-      likesCount: '5k',
-      commentsCount: '2k'
+      profileRole: 'Creator'
     },
     {
       imagePath: 'assets/images/podcast/12577967_02.jpg',
@@ -40,12 +33,7 @@ export class TrendingEpisodesComponent {
       description: 'Lorem Ipsum dolor sit amet consectetur',
       profileImage: 'assets/images/profile/handsome-asian-man-listening-music-through-headphones.jpg',
       profileName: 'William',
-      profileRole: 'Vlogger',
-      listensCount: '500k',
-      likesCount: '15k',
-      commentsCount: '10k'
+      profileRole: 'Vlogger'
     }
   ];
-
-
 }
