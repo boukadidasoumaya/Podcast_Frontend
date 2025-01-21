2f75f0beeb577ecff8b8f5f4f36b46a2c64c14f5
mabahej <mabahej.benhassine@insat.ucar.tn>
2025-01-21 18:54:50 +0100

    finished profil swiper

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,10 @@ import { TopicsComponent } from './components/topics/topics.component';
 import { TestComponent } from './components/test/test.component';
 import { LatestEpisodesComponent } from './components/latest-episodes/latest-episodes.component';
 import { HomeComponent } from './home/home.component';
+import { ProfilComponent } from './components/profil/profil.component';
 export const routes: Routes = [
   {path:'latest-episodes',component:LatestEpisodesComponent},
+  {path:'profil',component:ProfilComponent},
   {path:'swiper',component:TestComponent},
   {path:'',component:HomeComponent},
 ];
