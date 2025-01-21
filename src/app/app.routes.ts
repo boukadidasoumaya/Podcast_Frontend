a9d9110b0be9448e188473fa6a421ebed4ebba7f
AhmedTrigui137 <123263433+AhmedTrigui137@users.noreply.github.com>
2025-01-21 12:53:53 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardEpisodeComponent } from './components/card-episode/card-episode.component';
 import { NgModule } from '@angular/core';
 import { TrendingEpisodesComponent } from './components/trending-episodes/trending-episodes.component';
-
+import { LatestEpisodesComponent } from './components/latest-episodes/latest-episodes.component';
+import { LoginComponent } from './components/login/login.component';
 export const routes: Routes = [
-  {path:'',component:TrendingEpisodesComponent}
+  {path:'trending',component:TrendingEpisodesComponent},
+  {path:'',component:LatestEpisodesComponent},
+  {path: 'latest-episodes', component: LatestEpisodesComponent },
+  {path: 'login', component: LoginComponent }
 ];
 
