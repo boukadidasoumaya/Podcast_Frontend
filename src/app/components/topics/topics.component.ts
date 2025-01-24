df5886ad46522314631eeb8a527986ae6e1ef83e
mabahej <mabahej.benhassine@insat.ucar.tn>
2025-01-24 21:59:05 +0100

    missing comment section

--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SwiperComponent } from "../swiper/swiper.component";
 import { CommonModule } from '@angular/common';
 import { Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-topics',
   standalone: true,
-  imports: [ CommonModule],
+  imports: [ CommonModule, RouterLink],
   templateUrl: './topics.component.html',
   styleUrl: './topics.component.css'
 })
@@ -15,8 +16,11 @@ export class TopicsComponent {
   totalItems = 0;
   @Input() image: string = ''; // Input for image source
   @Input() title: string = ''; // Input for title
+  @Input() customtext: string = ''; // Input for title
   @Input() episodes: number = 0; // Input for episodes (optional)
-  
+  @Input() podcastId: number=0;
+@Input() episodeId: number=0;
+
   pagedTopics = [
     { 
       title: 'Productivity', 
