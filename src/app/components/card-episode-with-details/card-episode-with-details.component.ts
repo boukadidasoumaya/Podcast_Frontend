83bc38e4e7de1f79bb73bcf86823b1fa3cb2f8e3
Boukadida Soumaya <123579979+boukadidasoumaya@users.noreply.github.com>
2025-01-27 09:29:12 +0100

    some changes in latest episode and home

--- src/app/components/card-episode-with-details/card-episode-with-details.component.ts
+++ src/app/components/card-episode-with-details/card-episode-with-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { SubscribeButtonComponent } from '../subscribe-button/subscribe-button.component';
 
 @Component({
   selector: 'app-card-episode-with-details',
   standalone: true,
-  imports: [],
+  imports: [SubscribeButtonComponent],
   templateUrl: './card-episode-with-details.component.html',
   styleUrl: './card-episode-with-details.component.css'
 })
